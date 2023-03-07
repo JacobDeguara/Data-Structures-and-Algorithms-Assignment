62e7de5486e11aed3a7592dbeeedad6bf080dc37
Jacob Deguara <jacob.deguara.20@um.edu.mt>
2023-03-07 15:57:47 +0100

    [BStree]Feature:Added Insert and Display Functions

--- BStree.cpp
+++ BStree.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -14,13 +15,18 @@ class BStree
 {
 private:
     struct BSnode *root;
+    void Displaytree2(struct BSnode *root, int n);
 
 public:
     struct BSnode *Create_New_Node(int data);
     BStree(int HeadData);
+    BStree(vector<int> ListData);
 
     void Delete_Tree(struct BSnode *root);
     ~BStree();
+
+    struct BSnode *Insert_New_Node(struct BSnode *root, int data);
+    void Displaytree(struct BSnode *root);
 };
 
 struct BSnode *BStree::Create_New_Node(int data)
@@ -54,7 +60,72 @@ BStree::BStree(int HeadData)
     root = Create_New_Node(HeadData);
 }
 
+BStree::BStree(vector<int> ListData)
+{
+    root = Create_New_Node(ListData[0]);
+
+    for (size_t i = 1; i < ListData.size(); i++)
+    {
+        // insert New Nodes;
+    }
+}
+
+struct BSnode *BStree::Insert_New_Node(struct BSnode *root, int data)
+{
+    if (root == NULL)
+    {
+        root = Create_New_Node(data);
+        return root;
+    }
+
+    if (root->data < data)
+    {
+        root->left = Insert_New_Node(root->left, data);
+    }
+    else
+    {
+        root->right = Insert_New_Node(root->right, data);
+    }
+
+    return root;
+}
+
 BStree::~BStree()
 {
     Delete_Tree(root);
+}
+
+void BStree::Displaytree(struct BSnode *root)
+{
+    cout << L"▷" << root->data << endl;
+
+    if (root->right != NULL)
+    {
+        Displaytree2(root->right, 0);
+    }
+    if (root->left != NULL)
+    {
+        Displaytree2(root->left, 0);
+    }
+    return;
+}
+
+void BStree::Displaytree2(struct BSnode *root, int height)
+{
+    for (size_t i = 0; i < (height); i++)
+    {
+        cout << " ";
+    }
+
+    cout << L"┗▷" << root->data << endl;
+
+    if (root->right != NULL)
+    {
+        Displaytree2(root->right, height + 1);
+    }
+    if (root->left != NULL)
+    {
+        Displaytree2(root->left, height + 1);
+    }
+    return;
 }
