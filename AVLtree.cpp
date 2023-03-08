52a96f8ced1316376f83c08fe4c087423213c8ef
Jacob Deguara <jacob.deguara.20@um.edu.mt>
2023-03-08 19:09:35 +0100

    [AVLtree]Feature: Rotations implemented

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -20,6 +20,12 @@ public:
     shared_ptr<struct AVLnode> Create_New_Node(int data);
     AVLtree(int HeadData);
 
+    shared_ptr<struct AVLnode> LLRotation(shared_ptr<struct AVLnode> root);
+    shared_ptr<struct AVLnode> RRRotation(shared_ptr<struct AVLnode> root);
+    shared_ptr<struct AVLnode> RLRotation(shared_ptr<struct AVLnode> root);
+    shared_ptr<struct AVLnode> LRRotation(shared_ptr<struct AVLnode> root);
+
+    shared_ptr<struct AVLnode> getRoot();
     ~AVLtree() = default;
 };
 
@@ -34,4 +40,34 @@ shared_ptr<struct AVLnode> AVLtree::Create_New_Node(int data)
 AVLtree::AVLtree(int HeadData)
 {
     root = Create_New_Node(HeadData);
+}
+
+shared_ptr<struct AVLnode> AVLtree::LLRotation(shared_ptr<struct AVLnode> root)
+{
+    auto rootLeft = root->left;
+    root->left = rootLeft->right;
+    rootLeft->right = root;
+    return rootLeft;
+}
+shared_ptr<struct AVLnode> AVLtree::RRRotation(shared_ptr<struct AVLnode> root)
+{
+    auto rootRight = root->right;
+    root->right = rootRight->left;
+    rootRight->left = root;
+    return rootRight;
+}
+shared_ptr<struct AVLnode> AVLtree::RLRotation(shared_ptr<struct AVLnode> root)
+{
+    root->right = LLRotation(root->right);
+    return RRRotation(root);
+}
+shared_ptr<struct AVLnode> AVLtree::LRRotation(shared_ptr<struct AVLnode> root)
+{
+    root->left = RRRotation(root->left);
+    return LLRotation(root);
+}
+
+shared_ptr<struct AVLnode> AVLtree::getRoot()
+{
+    return root;
 }
